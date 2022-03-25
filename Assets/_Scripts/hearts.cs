bd2010ab6dca60fad08c87360d5cb1d05a9cf5d7
fernandocfbf <fernandocfbf@al.insper.edu.br>
2022-03-25 18:56:16 -0300

    feat: change heart velocity

--- Assets/_Scripts/hearts.cs
+++ Assets/_Scripts/hearts.cs
@@ -6,16 +6,16 @@ using UnityEngine;
 public class hearts : MonoBehaviour
 {
     GameManager gm;
-    public float speed = 10.0f;
+    public float speed = 2.0f;
     private Rigidbody2D rb;
     private Vector2 screenBounds;
     // Start is called before the first frame update
     void Start()
-    {    gm = GameManager.GetInstance();
+    {    
+        gm = GameManager.GetInstance();
         rb = this.GetComponent<Rigidbody2D>();
-        rb.velocity = new Vector2(-speed,0); //right to left (-speed,0)
-        //descrive de limites da tela
-       screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        rb.velocity = new Vector2(-4.0f,0); //right to left (-speed,0)
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
     }
 
     // Update is called once per frame
