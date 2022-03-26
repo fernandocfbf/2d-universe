6cc0d78fe88093ddddd0c1343f66b1006936e701
fernandocfbf <fernandocfbf@al.insper.edu.br>
2022-03-26 03:41:36 -0300

    feat: adding capsules to the game

--- Assets/_Scripts/CapsuleContent.cs
+++ Assets/_Scripts/CapsuleContent.cs
@@ -2,34 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-public class hearts : MonoBehaviour
-{
+public class CapsuleContent : MonoBehaviour{
     GameManager gm;
     public float speed = 2.0f;
+    public float rotateSpeed = 10.0f;
     private Rigidbody2D rb;
     private Vector2 screenBounds;
-    // Start is called before the first frame update
-    void Start()
-    {    
+    private GameObject Player;
+
+    void Start(){
         gm = GameManager.GetInstance();
         rb = this.GetComponent<Rigidbody2D>();
-        rb.velocity = new Vector2(-4.0f,0);
+        rb.velocity = new Vector2(-2.0f,0);
         screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    void Update(){
         if(transform.position.x < -2*screenBounds.x ){
             Destroy(this.gameObject);
         }
-        
     }
+
     private void OnTriggerEnter2D(Collider2D collision){
         if(collision.CompareTag("Player")){
+            Player = GameObject.FindWithTag("Player");
+            Player.GetComponent<PlayerController>().capsules ++;
             Destroy(gameObject);
-            gm.lifes ++;
         }
     }
 }
