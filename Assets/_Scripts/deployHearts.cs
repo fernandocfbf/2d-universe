c7ca62bc41cd3ae969af4d6d70e1a18b672e7bed
Gabrielamb2 <gabrielamb2@al.insper.edu.br>
2022-03-27 10:14:11 -0300

    Coracoes devem aparecer mais raramente

--- Assets/_Scripts/deployHearts.cs
+++ Assets/_Scripts/deployHearts.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 //fonte: https://pressstart.vip/tutorials/2018/09/25/58/spawning-obstacles.html
 public class deployHearts : MonoBehaviour
 {   
-    public GameObject asteroidPrefab;
+    public GameObject heartPrefab;
     public float respawnTime = 2.0f;
     private Vector2 screenBounds;
     
@@ -16,19 +16,25 @@ public class deployHearts : MonoBehaviour
     {   
        gm = GameManager.GetInstance();
         screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
-        StartCoroutine(asteroidWave());
+        StartCoroutine(heartWave());
     }
     
-    private void spawnEnemy(){
-        GameObject a = Instantiate(asteroidPrefab) as GameObject;
+    private void spawnHeart(){
+        GameObject a = Instantiate(heartPrefab) as GameObject;
         a.transform.position = new Vector2(screenBounds.x, Random.Range(-screenBounds.y, screenBounds.y));
     }
+    private int TryLucky(){
+        int randomNumber = Random.Range(0, 2); //10% of chance
+        Debug.Log(randomNumber); 
+        if (randomNumber == 1) return 1;
+        return 0;
+    }
 
-    IEnumerator asteroidWave(){
+    IEnumerator heartWave(){
         while(true){
             yield return new WaitForSeconds(respawnTime);
             if (gm.gameState == GameManager.GameState.GAME){
-                spawnEnemy();
+                if(TryLucky() == 1)spawnHeart();
             }
         }
     }
